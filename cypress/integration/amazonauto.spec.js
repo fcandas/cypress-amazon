993920084dbcecbc428685f4a7432feeb2bb9dee
fcandas <fatihcandas@gmail.com>
2021-10-28 00:01:07 +0300

    commit
    commit

--- cypress/integration/amazonauto.spec.js
+++ cypress/integration/amazonauto.spec.js
@@ -4,4 +4,12 @@ describe('Amazon Automation Test', () =>{
         cy.visit('https://www.amazon.com/');
         cy.get('a[href="/ref=nav_logo"]').should('be.visible')
     });
+
+    it('(Search thing)', () => {
+        
+        cy.get('input[id="twotabsearchtextbox"]').should('be.visible').type('Samsung')
+        cy.get('input[id="nav-search-submit-button"]').should('be.visible').click()
+
+    });
+    
 })
