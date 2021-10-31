37ac6ca756ab932810609ee5b0dba29dfeffea0c
fcandas <fatihcandas@gmail.com>
2021-10-31 23:12:00 +0300

    31st of oct commit
    31st of oct commit

--- cypress/integration/amazonauto.spec.js
+++ cypress/integration/amazonauto.spec.js
@@ -1,22 +1,20 @@
 describe('Amazon Automation Test', () =>{
 
-    it('(Open Amazon.com)', () => {
-        cy.visit('/');
-        cy.get('a[href="/ref=nav_logo"]').should('be.visible')
+    it('Go To Amazon.com', () => {
+
+       cy.goToPage();
+
     });
 
-    it('(Search thing)', () => {
+    it('Search thing', () => {
         
-        cy.get('input[id="twotabsearchtextbox"]').should('be.visible').type('Samsung')
-        cy.get('input[id="nav-search-submit-button"]').should('be.visible').click()
+        cy.searchThing();
 
     });
 
     it('Featured listing', () => {
 
-        
-        cy.get('span[id="a-autoid-0-announce"]').should('be.visible').click()
-        cy.get('a[id="s-result-sort-select_1"]').should('be.visible').click()
+        cy.featuredListing();
 
     });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,13 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
+
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
@@ -23,3 +14,24 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('goToPage', () => {
+
+    cy.visit('/');
+    cy.get('a[href="/ref=nav_logo"]').should('be.visible')
+})
+
+Cypress.Commands.add('searchThing', () => {
+
+    cy.get('input[id="twotabsearchtextbox"]').should('be.visible').type('Samsung')
+    cy.get('input[id="nav-search-submit-button"]').should('be.visible').click()
+
+})
+
+
+Cypress.Commands.add('featuredListing', () => {
+
+    cy.get('span[id="a-autoid-0-announce"]').should('be.visible').click()
+    cy.get('a[id="s-result-sort-select_1"]').should('be.visible').click()
+})
+
